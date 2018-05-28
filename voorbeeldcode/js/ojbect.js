a26d392fe277b3afc4998d3bac85b65d082d1ca7
Peter Kassenaar <info@kassenaar.com>
2018-05-28 12:43:43 +0200

    voorbeeld objecten (klanten en werknemers) bijgewerkt

--- voorbeeldcode/js/ojbect.js
+++ voorbeeldcode/js/ojbect.js
@@ -16,3 +16,25 @@ var fruitObject = {
 	}
 };
 document.getElementById('divResult').innerHTML = fruitObject.toonNaam();
+
+// Een array met klantgegevens.
+// Elke klant is een object
+var klanten = [
+	{ naam: 'Facebook', werknemers: 20000 },
+	{ naam: 'Google', werknemers: 35000 },
+	{ naam: 'Microsof', werknemers: 87000 },
+	{ naam: 'MinDef', werknemers: 12000 },
+	{ naam: 'BuZa', werknemers: 6000 }
+];
+// Klantgegevens tonen in de User Interface
+
+for (var i = 0; i < klanten.length; i++) {
+	// Hier, binnen de lus, een nieuw ListItem samenstellen
+	var listItem =
+		'<li>' +
+		klanten[i].naam +
+		', werknemers:' +
+		klanten[i].werknemers +
+		'</li>';
+	document.getElementById('lijstKlanten').innerHTML += listItem;
+}
