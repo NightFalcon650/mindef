be335f7042c4c7db57413d6f187d881015c9758c
Peter Kassenaar <info@kassenaar.com>
2018-05-29 12:51:49 +0200

    nieuwe voorbeelden Toggle() toegevoegd

--- voorbeeldcode/js/08-callback.js
+++ voorbeeldcode/js/08-callback.js
@@ -1,21 +1,27 @@
 // 08-callback.js
 $(document).ready(function() {
 	// Laat de voorwaarden in beginsel verborgen
-	
 
 	$('#btnVerberg').on('click', function() {
-		// hide wordt nu 'asynchroon' uitgevoerd
-		$('#voorwaarden').hide('slow', function() {
-			// Doe dit pas na afloop.
-			alert('De voorwaarden zijn nu verborgen');
-		});
+		if ($('#voorwaarden').is(':hidden')) {
+			alert('de voorwaarden zijn al verborgen');
+		} else {
+			// hide wordt nu 'asynchroon' uitgevoerd
+			$('#voorwaarden').hide('slow', function() {
+				// Doe dit pas na afloop.
+				alert('De voorwaarden zijn nu verborgen');
+			});
+		}
 		// ga pas een function uitvoeren
 		// als de animatie gereed is.
-		// dit heet een CALLBACK-function
-
-		// alert('De voorwaarden zijn verborgen');
+		// dit heet een CALLBACK-function;
 	});
 	$('#btnToon').on('click', function() {
 		$('#voorwaarden').show('slow');
+	});
+
+	// Dit is voor de knop Toggle zichtbaar/onzichtbaar
+	$('#btnToggle').on('click', function() {
+		$('#voorwaarden').toggle();
 	});
 });
