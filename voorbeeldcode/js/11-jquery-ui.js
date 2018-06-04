0e0fa9200aa419ee0b08aa953dada145874c01f3
Peter Kassenaar <info@kassenaar.com>
2018-06-04 11:03:25 +0200

    nieuwe jQuery-code en slides Captivate toegevoegd

--- voorbeeldcode/js/11-jquery-ui.js
+++ voorbeeldcode/js/11-jquery-ui.js
@@ -24,10 +24,22 @@ $(document).ready(function() {
 		min: 0,
 		max: 100,
 		value: 50,
-		step: 10,
 		slide: function(event, ui) {
 			$('#sliderResult').html(ui.value);
+			$('#inputBestel').val(ui.value);
 		}
 	};
 	$('#mySlider').slider(sliderOptions);
+
+	$('#btnInstellen').on('click', function(e) {
+		// 1. 'e' is hier het event dat door de browser wordt gegenereerd.
+		// Het de standaardactie voor knoppen, namelijk het versturen van het formulier.
+		// Dat wil je hier voorkomen,
+		// Omdat je een eigen waarde wilt invullen in het bestelvak.
+		e.preventDefault();
+
+		// 2. Voer je eigen handelingen uit, IN PLAATS VAN versturen.
+		var aantalItems = $('#mySlider').slider('value'); // Dit is nu een <input type="hidden" ...>
+		$('#inputBestel').val(aantalItems);
+	});
 });
