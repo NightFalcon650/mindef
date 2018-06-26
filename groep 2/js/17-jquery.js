b019b1476b1e7b1cce7c672d4a519fc53d90fb6a
Peter Kassenaar <info@kassenaar.com>
2018-06-26 14:38:58 +0200

    jQuery voorbeeld uitgebreid

--- groep 2/js/17-jquery.js
+++ groep 2/js/17-jquery.js
@@ -1,52 +1,67 @@
 // 17-jquery.js
 
-// $('body').css('background-color', 'blue');
+// algemene variabelen, buiten de document.ready-function
 
-// jQuery gebruiken: gebruik het dollarteken.
-// OF: jQuery(...selectie...)
+// Best practice: gebruik globale 'Namespace' om variabelen af te schermen
+// voor per ongeluk overschrijven door andere variabelen.
+// Dit heet het 'Object Literal Design Pattern';
+var mindefVars = {
+	fruitArray: []
+};
 
-var fruitArray = [];
+$(document).ready(function() {
+	// $('body').css('background-color', 'blue');
 
-// Haal referentie op naar de knop en voeg Click-eventlistener toe
-$('#fruitToevoegen').on('click', function() {
-	// 1. Waarde ophalen uit het tekstvak met jQuery-opdracht .val()
-	var mijnFruit = $('#newFruit').val();
+	// jQuery gebruiken: gebruik het dollarteken.
+	// OF: jQuery(...selectie...)
 
-	// 2. Toevoegen/push op de array
-	fruitArray.push(mijnFruit);
+	// Haal referentie op naar de knop en voeg Click-eventlistener toe
+	$('#fruitToevoegen').on('click', function() {
+		// 1. Waarde ophalen uit het tekstvak met jQuery-opdracht .val()
+		var mijnFruit = $('#newFruit').val();
 
-	// 3. Loop over de array en voeg
-	// nieuw listItem toe voor elk fruit
-	renderList();
-});
+		// 2. Toevoegen/push op de array
+		mindefVars.fruitArray.push(mijnFruit);
 
-// 4. Gedrag aanhaken voor de Delete knop in elk listItem
-$(document).on('click', '.deleteBtn', function() {
-	// 4a. Zoek positie binnen de array op
-	var position = $(this).data('id');
+		// 3. Loop over de array en voeg
+		// nieuw listItem toe voor elk fruit
+		renderList();
+	});
 
-	// 4b. splits 1 item uit de array op gevonden positie
-	fruitArray.splice(position, 1);
+	// 4. Gedrag aanhaken voor de Delete knop in elk listItem
+	$(document).on('click', '.deleteBtn', function() {
+		// 4a. Zoek positie binnen de array op
+		var position = $(this).data('id');
 
-	// 4c. render de lijst opnieuw
-	renderList();
-});
+		// 4b. splits 1 item uit de array op gevonden positie
+		mindefVars.fruitArray.splice(position, 1);
 
-// 5. Lijst (opnieuw) renderen.
-function renderList() {
-	// 5a. Bestaande lijst leegmaken, met jQuery-opdracht .empty()
-	$('#listFruit').empty();
+		// 4c. render de lijst opnieuw
+		renderList();
+	});
 
-	// 5b. Standaard JavaScript for-lus
-	for (var i = 0; i < fruitArray.length; i++) {
-		// Nieuw listItem samenstellen, met Delete-knop
-		var listItem = '<li class="list-group-item">' + fruitArray[i];
-		listItem +=
-			'<button class="btn btn-danger btn-sm float-right deleteBtn" data-id="' +
-			i +
-			'">Delete</button>' +
-			'</li>';
-		// 5c. daadwerkelijk toevoegen, met jQuery-opdracht .append()
-		$('#listFruit').append(listItem);
+	// 5. Lijst (opnieuw) renderen.
+	function renderList() {
+		// 5a. Bestaande lijst leegmaken, met jQuery-opdracht .empty()
+		$('#listFruit').empty();
+
+		// 5b. Standaard JavaScript for-lus
+		for (var i = 0; i < mindefVars.fruitArray.length; i++) {
+			// Nieuw listItem samenstellen, met Delete-knop
+			var listItem =
+				'<li class="list-group-item">' + mindefVars.fruitArray[i];
+			listItem +=
+				'<button class="btn btn-danger btn-sm float-right deleteBtn" data-id="' +
+				i +
+				'">Delete</button>' +
+				'</li>';
+			// 5c. daadwerkelijk toevoegen, met jQuery-opdracht .append()
+			$('#listFruit').append(listItem);
+		}
 	}
-}
+
+	// 6. Klik op de knop en de voorwaarden worden verborgen:
+	$('#btnVoorwaarden').on('click', function() {
+		$('#voorwaarden').slideToggle(300);
+	});
+}); // end document ready
