094c1ca5b4201e13679922acc4d97ffd12969f66
Peter Kassenaar <info@kassenaar.com>
2018-07-05 14:45:56 +0200

    JavaScript formulieren toegevoegd

--- groep 2/js/20-html-formulieren.js
+++ groep 2/js/20-html-formulieren.js
@@ -5,15 +5,20 @@ $(document).ready(function() {
 
 	// Ga luisteren naar een change op de lijst met provincies
 	$('#provincies').on('change', function() {
+		// Als voor 'overig' is gekozen, laat dan de
+		// div zien waarin een andere provincie ingevuld kan worden.
 		if ($('#provincies').val() === 'other') {
 			$('#divProvincie').show();
 		} else {
+			// anders: verberg deze div indien zichtbaar.
 			$('#divProvincie').hide();
 		}
 	});
 
 	// Ga luisteren naar een change op de lijst met websites
 	$('#sites').on('change', function() {
+		// Open de url in een nieuw venster/tab
 		window.open($(this).val());
+		// mag ook : window.open($('#sites').val());
 	});
 });
