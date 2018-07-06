c4e99b64825e1b210d416773f62ae477b1f40de3
Peter Kassenaar <info@kassenaar.com>
2018-07-06 12:40:09 +0200

    voorbeeld Slider toegevoegd

--- groep 2/js/22-jquery-ui.js
+++ groep 2/js/22-jquery-ui.js
@@ -1,4 +1,23 @@
 // 22-jquery-ui.js
+
+var beginwaarde = 700;
+
 $(document).ready(function() {
+	// jQuery UI datepicker activeren.
 	$('#datumpicker').datepicker($.datepicker.regional['nl']);
+
+	$('#sliderValue').html(beginwaarde);
+
+	// Maak een configuratie object voor de slider
+	var sliderConfig = {
+		slide: function(event, ui) {
+			$('#sliderValue').html(ui.value);
+		},
+		min: 100,
+		max: 1000,
+		value: beginwaarde
+	};
+
+	// Slider activeren met een configuratie object
+	$('#slider').slider(sliderConfig);
 });
